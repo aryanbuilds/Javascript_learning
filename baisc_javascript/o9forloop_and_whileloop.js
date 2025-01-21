eee69abb9484f963a4edfbf9ac6f2d5bbd7168c6
aryanbuilds <raiaryan174@gmail.com>
2025-01-21 19:22:06 +0530

    modified:   baisc_javascript/o9forloop_and_whileloop.js

--- baisc_javascript/o9forloop_and_whileloop.js
+++ baisc_javascript/o9forloop_and_whileloop.js
@@ -1,10 +1,10 @@
-// // 02:52 for loop
+// //  for loop
 // // A for loop is used to repeat a block of code a known number of times.
 // for (let i = 0; i < 5; i++) {
 //     console.log("Iteration number: " + i);
 // }
 
-// // 8:40 if condition inside for loop
+// //  if condition inside for loop
 // // You can use an if condition inside a for loop to execute code only when certain conditions are met.
 // for (let i = 0; i < 5; i++) {
 //     if (i % 2 === 0) {
@@ -12,7 +12,7 @@
 //     }
 // }
 
-// // 10:50 for loop inside for loop
+// //  for loop inside for loop
 // // A for loop inside another for loop is called a nested loop. It is used to iterate over multi-dimensional data structures.
 // for (let i = 0; i < 3; i++) {
 //     for (let j = 0; j < 3; j++) {
@@ -20,14 +20,14 @@
 //     }
 // }
 
-// // 15:57 array inside for loop
+// //  array inside for loop
 // // You can loop through an array using a for loop to access each element.
 // const array = [1, 2, 3, 4, 5];
 // for (let i = 0; i < array.length; i++) {
 //     console.log("Element at index " + i + " is " + array[i]);
 // }
 
-// // 18:40 keyword in loop (break and continue)
+// // keyword in loop (break and continue)
 // // The 'break' keyword exits the loop immediately, and the 'continue' keyword skips the current iteration and moves to the next one.
 // for (let i = 0; i < 10; i++) {
 //     if (i === 5) {
@@ -38,3 +38,6 @@
 //     }
 //     console.log(i); // This will only log odd numbers less than 5
 // }
+
+//++++++++++++++++++++++ WHILE LOOP ++++++++++++++++++++++++++++
+
