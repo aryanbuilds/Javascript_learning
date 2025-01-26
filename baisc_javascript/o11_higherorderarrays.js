00fb7a448269f7b91dffe9e40cca26a9416be0dd
aryanbuilds <raiaryan174@gmail.com>
2025-01-26 18:33:28 +0530

    modified:   baisc_javascript/o11_higherorderarrays.js

--- baisc_javascript/o11_higherorderarrays.js
+++ baisc_javascript/o11_higherorderarrays.js
@@ -1,34 +1,49 @@
-// 01:42 Putting objects inside arrays and accessing values through loops
-const students = [
-    { name: 'John', age: 20 },
-    { name: 'Jane', age: 22 },
-    { name: 'Jim', age: 21 }
-];
+// Putting objects inside arrays and accessing values through loops
+// const students = [
+//     { name: 'John', age: 20 },
+//     { name: 'Jane', age: 22 },
+//     { name: 'Jim', age: 21 }
+// ];
 
-students.forEach(function(student) {
-    console.log(student.name + ' is ' + student.age + ' years old');
-});
 
-// 05:19 Introduction to maps in JavaScript
+//FOR in DESCRIPTION   =>  it works for ARRAY(returns keys i.e index value of elements) and OBJECT but not works on MAP
+//FOR of DESCRIPTION   =>  it works for ARRAY and MAP[ key, value] but not works on OBJECT.
+//FOR each DESCRIPTION =>  it works for ARRAY, OBJECT and MAP.
+
+
+// // forEach loop
+// students.forEach(function(student) {
+//     console.log(student.name + ' is ' + student.age + ' years old');
+// });
+
+// // Using for...of loops in JavaScript
+// for (const stu of students) {
+//     console.log(stu.name + ' is ' + stu.age + ' years old');
+// }
+// ++++++++++++++++++++Introduction to maps in JavaScript+++++++++++++++++++++
 const map = new Map();
-map.set('name', 'John');
+map.set('name', 'John'); // map.set(key, value)
 map.set('age', 20);
 
 console.log(map.get('name')); // John
 console.log(map.get('age')); // 20
+// using for of loop
 
+for (const [key, value] of map) {
+    console.log(key + ': ' + value);
+} // name: John, age: 20
 // 07:10 Setting values in an object and accessing data from objects
 const person = {
     name: 'Jane',
     age: 22
 };
 
-person.city = 'New York';
-console.log(person);
+person.city = 'New York'; // Adding a new property to the object
+console.log(person); // {name: "Jane", age: 22, city: "New York"}
 
-// 11:02 Using high order array loops in JavaScript
+// +++++++++++++++++ Using high order array loops in JavaScript+++++++++++++++++++++
 const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map(function(num) {
+const doubled = numbers.map(function (num) {
     return num * 2;
 });
 console.log(doubled); // [2, 4, 6, 8, 10]
@@ -50,10 +65,10 @@ for (let i = 0; i < numbers.length; i++) {
 //   console.log(map[i]);
 // }
 
-// Correct way to iterate over a Map
-map.forEach(function(value, key) {
-    console.log(key + ': ' + value);
-});
+// // Correct way to iterate over a Map
+// map.forEach(function(value, key) {
+//     console.log(key + ': ' + value);
+// });
 
 // 23:36 Functions in JavaScript automatically know the scope and parameters
 function greet(name) {
